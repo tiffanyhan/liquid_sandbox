d29d91133849f1eed67f8f4656a6183cc07c0955
Tiffany Han <tiffany@lye.com>
2018-08-08 12:24:54 -0700

    pass parsed json into compiled template

--- launcher.rb
+++ launcher.rb
@@ -1,12 +1,17 @@
 require 'liquid'
 require 'launchy'
+require 'json'
 
 f = open('./content.liquid')
 content = f.read
 
 @template = Liquid::Template.parse(content) # Parses and compiles the template
 
-html = @template.render('data' => '')
+f = open('./response.json')
+json = f.read
+dict = JSON.parse(json)
+
+html = @template.render(dict)
 f = File.new('./index.html', 'w')
 f.write(html)
 
